1716bfcb563348081cf0d3e422a97faf315d1b79
Amittras Pal <72859660+amittras-pal@users.noreply.github.com>
2025-01-21 06:30:50 +0000

    Update journal entries view to calendar type.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layout/index.tsx";
 import Configurator from "./modules/configurator/index.tsx";
 import Tracker from "./modules/tracker/index.tsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
--- src/modules/tracker/Entries.tsx
+++ src/modules/tracker/Entries.tsx
@@ -1,32 +1,71 @@
-import { Box, Text } from "@mantine/core";
-import { GoalConfig } from "../../context/context";
+import { Alert, Flex, Indicator, Text } from "@mantine/core";
+import { Calendar } from "@mantine/dates";
 import dayjs from "dayjs";
+import { Entry, GoalConfig } from "../../context/context";
+import { useState } from "react";
+import { IconInfoCircle } from "@tabler/icons-react";
 
 export default function Entries(props: { data: GoalConfig }) {
+  const [entry, setEntry] = useState<Entry | null>(null);
   return (
-    <>
-      {(props.data.entries?.length ?? 0) > 0 ? (
-        props.data.entries?.map((entry) => (
-          <Box
-            key={entry.id}
-            style={(theme) => ({
-              padding: theme.spacing.xs,
-              backgroundColor: theme.colors.dark[6],
-              marginBottom: theme.spacing.sm,
-              borderRadius: theme.radius.sm,
-            })}
-          >
-            <Text>Added on {dayjs(entry.date).format("MMMM DD")}</Text>
-            <Text fz="sm" c="dimmed" fs="italic">
-              {entry.notes}
-            </Text>
-          </Box>
-        ))
-      ) : (
-        <Text fs="italic" c="red">
-          No Entries added in this goal yet!!
+    <Flex direction="column" align="center" gap="md">
+      <Calendar
+        style={(theme) => ({
+          border: `1px solid ${theme.colors.gray[7]}`,
+          padding: theme.spacing.xs,
+          borderRadius: theme.radius.md,
+        })}
+        getDayProps={(calendarDay) => {
+          const entry = props.data.entries?.find((item) =>
+            dayjs(item.date).isSame(calendarDay, "day")
+          );
+          return {
+            style: (theme) => ({
+              backgroundColor: entry ? theme.colors.indigo[8] : "transparent",
+              color: theme.white,
+            }),
+            onClick: () => {
+              if (entry?.notes) setEntry(entry);
+              else setEntry(null);
+            },
+          };
+        }}
+        renderDay={(calendarDay) => {
+          const entry = props.data.entries?.find((item) =>
+            dayjs(item.date).isSame(calendarDay, "day")
+          );
+          return (
+            <Indicator
+              size={10}
+              withBorder
+              position="top-end"
+              color="red"
+              offset={-5}
+              disabled={!entry?.notes}
+            >
+              <div>{dayjs(calendarDay).date()}</div>
+            </Indicator>
+          );
+        }}
+      />
+      <Text fz="sm">
+        Total Entries So Far:{" "}
+        <Text component="span" fw="bold">
+          {props.data.entries?.length ?? 0}
         </Text>
+      </Text>
+      {entry && (
+        <Alert
+          variant="light"
+          color="indigo"
+          withCloseButton
+          onClose={() => setEntry(null)}
+          title={`Notes for ${dayjs(entry.date).format('DD MMM')}`}
+          icon={<IconInfoCircle />}
+        >
+          {entry.notes}
+        </Alert>
       )}
-    </>
+    </Flex>
   );
 }
